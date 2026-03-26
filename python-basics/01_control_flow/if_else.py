ca76d2c24ce5156e1b3b8c15aa0f7c0b7c31b30c
5JUHEE <122730678+5JUHEE@users.noreply.github.com>
2026-03-26 18:31:46 +1100

    Create if_else.py

---
+++ python-basics/01_control_flow/if_else.py
@@ -0,0 +1,22 @@
+# if_else.py
+# Topic: Control Flow - If Statement
+
+"""
+This example demonstrates how to use if-else conditions in Python.
+"""
+
+a = 3
+b = 5
+
+if a > b:
+    print("a is bigger than b")
+else:
+    print("b is bigger than a")
+
+# Another example
+num = 10
+
+if num % 2 == 0:
+    print("Even number")
+else:
+    print("Odd number")
