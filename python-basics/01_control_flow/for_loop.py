54b34e8053f1b96c003d4318942f1a49b6f5511a
5JUHEE <122730678+5JUHEE@users.noreply.github.com>
2026-03-26 18:48:18 +1100

    Update for_loop.py

--- python-basics/01_control_flow/for_loop.py
+++ python-basics/01_control_flow/for_loop.py
@@ -1,12 +1,16 @@
 # for_loop.py
 # Topic: Control Flow - For Loop
 
+# 1: basic range
 for i in range(5):
     print(i)
 
-# iterate over list
-lst = [1,2,3]
-
+# 2: iterate list
+lst = [1, 2, 3]
 for item in lst:
     print(item)
+
+# 3: index + value
+for idx, val in enumerate(lst):
+    print(idx, val)
 
